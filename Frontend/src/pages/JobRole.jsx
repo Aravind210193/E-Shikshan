8a43ab52f6fa87eb5759e1b4809df4e511f61185
swarnaraja-sekhar <bhuchiki12@gmail.com>
2025-07-24 16:13:11 +0530

    structure

---
+++ Frontend/src/pages/JobRole.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobRole = () => {
+  return (
+    <div>JobRole</div>
+  )
+}
+
+export default JobRole
---
+++ Frontend/src/pages/ResumeBuilding.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResumeBuilding = () => {
+  return (
+    <div>ResumeBuilding</div>
+  )
+}
+
+export default ResumeBuilding
